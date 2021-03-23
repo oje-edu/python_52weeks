cee763ccf4202087ffb29eebb8c344af1b41a18e
oje-edu <Thorsten.Oje@gfn.education>
2021-03-23 19:12:40 +0100

    initital

---
+++ m01_basics/l_01_device.py
@@ -0,0 +1,27 @@
+from pprint import pprint
+
+# DICTIONARY representing a device
+device = {
+    "name": "sbx-n9kv-ao",
+    "vendor": "cisco",
+    "model": "Nexus9000 C9300v Chassis",
+    "os": "nxos",
+    "version": "9.3(3)",
+    "ip": "10.1.1.1",
+}
+
+# SIMPLE PRINT
+print("\n_____ SIMPLE PRINT ____________________")
+print("device:", device)
+print("device name:", device["name"])
+
+# PRETTY PRINT
+print("\n_____ PRETTY PRINT _____________________")
+pprint(device)
+
+# FOR LOOP, NICELY FORMATTED PRINT
+print("\n_____ FOR LOOP, USING F-STRING _______________")
+for key, value in device.items():
+    print(f"{key:>16s} : {value}")
+
+
