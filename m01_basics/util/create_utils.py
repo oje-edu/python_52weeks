4f21a0926f2c0885ce35d891006f82c6d30d85b7
oje-edu <thorsten.oje@gfn.education>
2021-04-18 17:24:24 +0200

    refactor

--- m01_basics/util/create_utils.py
+++ m01_basics/util/create_utils.py
@@ -1,6 +1,33 @@
 from random import choice
 import string
 
+def create_device(device_index, subnet_index):
+
+    device = dict()
+
+    # RANDOM DEVICE NAME
+    device["name"] = (
+            choice(["IHK", "BRD", "no4", "r10"])
+            + choice(["L", "U"])
+            + choice(string.ascii_letters)
+    )
+    # RANDOM VENDOR FROM CHOICE OF CISCO, JUNIPER; ARISTA
+    device["vendor"] = choice(["cisco", "juniper", "arista"])
+    if device["vendor"] == "cisco":
+        device["os"] = choice(["ios", "iosxe", "iosxr", "nexus"])
+        device["version"] = choice(["12.1(T).04", "14.07X", "8.12(S).010", "20.25"])
+    elif device["vendor"] == "juniper":
+        device["os"] = "junos"
+        device["version"] = choice(["J6.23.1", "8.43.12", "6.45", "6.03"])
+    elif device["vendor"] == "arista":
+        device["os"] = "eos"
+        device["version"] = choice(["2.45.1F", "2.55", "2.92.145", "3.01"])
+
+    device["ip"] = "10.0." + str(subnet_index) + "." + str(device_index)
+
+    return device
+
+
 def create_devices(num_devices=1, num_subnets=1):
     # CREATE LIST OF DEVICES
     created_devices = list()
@@ -9,33 +36,16 @@ def create_devices(num_devices=1, num_subnets=1):
         print("ALTA: hast du deinen Fachinformatiker bei der IHK gewonnen ?? viel zu viele Geräte/Subnetze angefordert")
         return created_devices
 
+    print("beginning device creation")
     for subnet_index in range(1, num_subnets + 1):
 
         for device_index in range(1, num_devices + 1):
 
-            # CREATE DEVICE DICTIONARY
-            device = dict()
-
-            # RANDOM DEVICE NAME
-            device["name"] = (
-                    choice(["IHK", "BRD", "no4", "r10"])
-                    + choice(["L", "U"])
-                    + choice(string.ascii_letters)
-            )
-            # RANDOM VENDOR FROM CHOICE OF CISCO, JUNIPER; ARISTA
-            device["vendor"] = choice(["cisco", "juniper", "arista"])
-            if device["vendor"] == "cisco":
-                device["os"] = choice(["ios", "iosxe", "iosxr", "nexus"])
-                device["version"] = choice(["12.1(T).04", "14.07X", "8.12(S).010", "20.25"])
-            elif device["vendor"] == "juniper":
-                device["os"] = "junos"
-                device["version"] = choice(["J6.23.1", "8.43.12", "6.45", "6.03"])
-            elif device["vendor"] == "arista":
-                device["os"] = "eos"
-                device["version"] = choice(["2.45.1F", "2.55", "2.92.145", "3.01"])
-            device["ip"] = "10.0." + str(subnet_index) + "." + str(device_index)
-
+            device = create_device(device_index, subnet_index)
             created_devices.append(device)
+
+            print(".", end="")
+    print("completed device creation")
     return created_devices
 
 def create_network(num_devices=1, num_subnets=1):
